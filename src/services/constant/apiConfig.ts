9c93d59acf099a8f79de6541b4db189497988f00
Khắc Bảo <87173948+devbaoo@users.noreply.github.com>
2025-09-11 14:20:26 +0700

    Update apiConfig.ts

--- src/services/constant/apiConfig.ts
+++ src/services/constant/apiConfig.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://clownfish-app-u7jdf.ondigitalocean.app/api";
+export const BASE_URL = "https://octopus-app-ebuoh.ondigitalocean.app/api";
 // export const BASE_URL = "http://localhost:8080/api";
 //auth
 export const LOGIN_ENDPOINT = `${BASE_URL}/auth/login`;
